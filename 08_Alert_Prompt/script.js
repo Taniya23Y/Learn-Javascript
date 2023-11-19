006b7df89b0c1280b8453457856e25f5d6c2363e
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2023-11-19 08:41:45 +0530

    Add files via upload

---
+++ 08_Alert_Prompt/script.js
@@ -0,0 +1,11 @@
+// Alert : Display an alert message on the page
+window.alert("Please slow down");
+console.log("This is simple log");
+console.error("This is simple log");
+console.warn("This is simple log");
+
+
+// Prompt: displays dialog box that asks user for some input.
+
+let firstName = prompt("enter your name: ");
+console.log(firstName);
