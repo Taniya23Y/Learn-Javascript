420651774584d34dddfa2d8bee7539b81277b0de
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-01-28 21:55:36 +0530

    Add files via upload

---
+++ 00_AdvacedTopic.txt/app.js
@@ -0,0 +1,17 @@
+// const variable
+
+// Constant Arrays
+//--> you can change the elements of constant array.
+
+// Example:
+
+//You can create a constant array:
+const cars = ["Thar", "Indigo", "BMW"];
+console.log(cars);
+//You can change an element:
+cars[0] = "Saab";
+console.log(cars);
+
+//You can add an element:
+cars.push("Audi");
+console.log(cars);
