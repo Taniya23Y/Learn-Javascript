5d06ddc78ad78ec4dcc718589b87c37a12685d1f
Taniya Y <128471154+Taniya23Y@users.noreply.github.com>
2024-02-25 17:35:40 +0530

    Add files via upload

---
+++ 02_Variable/app1.js
@@ -0,0 +1,22 @@
+// `let and const declarations are Hoisted` this are in the temporal dead zone at the time
+
+/*
+   Temporal  Dead Zone (TDZ) is a term used by JavaScript developers to describe a period of time 
+   during which variables that have been declared using let or const. 
+   The Temporal Dead Zone is a unique feature introduced with the and declarations in JavaScript. 
+   It's the period between entering a scope and the actual declaration 
+   of a variable within that scope. During this phase, the variable exists but is inaccessible.
+   Accessing it before the declaration results in a ReferenceError.
+
+   +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+   The best way to avoid this temporal dead zone is to always put your declarations and initialization at 
+   the top of code as soon as your codes starts running it hits the initialization part at the first.
+*/
+
+
+// console.log(b); // undefined
+// console.log(a); // ReferenceError: Cannot access 'a' before initialization
+
+let a = 10;
+console.log(a); 
+var b = 100;
