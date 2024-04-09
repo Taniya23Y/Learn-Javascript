0e603e839735328e8ac5aa75328f3f4c4391d743
Taniya Yadav <128471154+Taniya23Y@users.noreply.github.com>
2024-04-09 09:45:32 +0530

    Add files via upload

---
+++ 42_Some_extra_stuffs/01_Crack Frontend Interview YT by Akshay Saini/02_Season - 2_Ace Machine Coding Round/Ep-1_Machine_Coding_Round.js
@@ -0,0 +1,25 @@
+//* Machine Coding Round Tips and Tricks
+
+/**
+ * Time Management is the key
+ * Learn writing from scratch
+ * Learn Editor Shortcuts
+ * Fetching data from API Calls
+ * Library Management (Ex. Loadash)
+ * DOM Manipulation
+ * Local Storage
+ * Searching
+ * Sorting
+ * Tagging
+ * Flexbox in css
+ * Responsiveness of App
+ * Thinking Recursively
+ * Is your code Modular?
+ * Is your code Scalable?
+ * Is your code Readable? Keep all files separate
+ * Keep check on time regularly
+ * No Distractions
+ * Test your Code
+ * Cherry on the cake, Impress interviewer
+ * --> Make a personal collection of syntax&pre made functionality. in any point, if You get confused you will refer that document.
+*/
